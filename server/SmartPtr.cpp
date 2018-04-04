b347a150633062d41d8c9456b49bebf6d40ed4a4
Eric Leonardo Lim <ericl619@gmail.com>
2018-04-05 06:18:48 +0800

    deleted unused files

--- server/SmartPtr.cpp
+++
@@ -1,6 +0,0 @@
-#include "SmartPtr.h"
-
-explicit SmartPtr::SmartPtr(T *p = NULL) { ptr = p;}
-SmartPtr::~SmartPtr() { delete(ptr); }
-T & SmartPtr::operator * () { return *ptr; }
-T * SmartPtr::operator -> () {return ptr; }
